5dca24bb92c37cc841bf656f37d3e6aae708bd7c
hippedorange_22 <saifkattingeri@gmail.com>
2022-04-18 19:39:35 +0530

    removed pubspec comments.

--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,6 @@ check() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
